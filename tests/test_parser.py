bc98fa5aea884ba254bf7e49f0f76cecae434b8f
PureFunctor <purefunctor@gmail.com>
2020-08-09 00:30:03 +0800

    Add comprehensive tests for regex patterns

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,33 @@
+import re
+
 from amalgam.parser import s_expression
+from amalgam.parser import IDENTIFIER_PATTERN, NUMERIC_PATTERN
+
+from hypothesis import given
+from hypothesis.strategies import integers, floats, fractions, from_regex
+
+
+_random_identifier = from_regex(fr"\A{IDENTIFIER_PATTERN}\Z")
+
+
+@given(_random_identifier)
+def test_identifier_pattern(identifier):
+    assert not re.match("[0-9]", identifier[0])
+
+
+@given(integers())
+def test_numeric_integer_pattern(integer):
+    assert re.match(NUMERIC_PATTERN, str(integer))
+
+
+@given(floats(allow_infinity=False, allow_nan=False))
+def test_numeric_floating_pattern(floating):
+    assert re.match(NUMERIC_PATTERN, str(floating))
+
+
+@given(fractions())
+def test_numeric_fraction_pattern(fraction):
+    assert re.match(NUMERIC_PATTERN, str(fraction))
 
 
 def test_s_expression_arithmetic_simple():
