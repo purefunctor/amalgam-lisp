da18e9d857fabbaee46ae46b19a5b562390b7ad5
PureFunctor <purefunctor@gmail.com>
2020-08-09 18:01:44 +0800

    Retire old tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -48,25 +48,25 @@ def test_s_expression_literals(identifier, literals):
     assert s_expression.parse(f"({identifier} {' '.join(literals)})") == ["(", identifier, *literals, ")"]
 
 
-def test_s_expression_arithmetic_simple():
-    assert s_expression.parse("(+ 21 42)") == ["(", "+", "21", "42", ")"]
-
-
-def test_s_expression_arithmetic_nested():
-    assert s_expression.parse("(+ 21 42 (+ 21 42))") == ["(", "+", "21", "42", ["(", "+", "21", "42", ")"], ")"]
-
-
-def test_s_expression_identifier_simple():
-    assert s_expression.parse("(foo 21 42)") == ["(", "foo", "21", "42", ")"]
-
-
-def test_s_expression_identifier_nested():
-    assert s_expression.parse("(foo 21 42 (bar 21 42))") == ["(", "foo", "21", "42", ["(", "bar", "21", "42", ")"], ")"]
-
-
-def test_s_expression_floating_numbers():
-    assert s_expression.parse("(foo 21.42 42.21)") == ["(", "foo", "21.42", "42.21", ")"]
-
-
-def test_s_expression_rational_numbers():
-    assert s_expression.parse("(foo 21/42 42/21)") == ["(", "foo", "21/42", "42/21", ")"]
+# def test_s_expression_arithmetic_simple():
+#     assert s_expression.parse("(+ 21 42)") == ["(", "+", "21", "42", ")"]
+#
+#
+# def test_s_expression_arithmetic_nested():
+#     assert s_expression.parse("(+ 21 42 (+ 21 42))") == ["(", "+", "21", "42", ["(", "+", "21", "42", ")"], ")"]
+#
+#
+# def test_s_expression_identifier_simple():
+#     assert s_expression.parse("(foo 21 42)") == ["(", "foo", "21", "42", ")"]
+#
+#
+# def test_s_expression_identifier_nested():
+#     assert s_expression.parse("(foo 21 42 (bar 21 42))") == ["(", "foo", "21", "42", ["(", "bar", "21", "42", ")"], ")"]
+#
+#
+# def test_s_expression_floating_numbers():
+#     assert s_expression.parse("(foo 21.42 42.21)") == ["(", "foo", "21.42", "42.21", ")"]
+#
+#
+# def test_s_expression_rational_numbers():
+#     assert s_expression.parse("(foo 21/42 42/21)") == ["(", "foo", "21/42", "42/21", ")"]
