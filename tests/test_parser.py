86757c5d14473cae93c98e0b1fe50e6bcc94a41d
PureFunctor <purefunctor@gmail.com>
2020-08-12 13:39:34 +0800

    Remove old tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -65,27 +65,3 @@ def test_s_expression_nested(l, r):
     te = ["(", "T", le, re, ")"]
 
     assert s_expression.parse(ts) == te
-
-
-# def test_s_expression_arithmetic_simple():
-#     assert s_expression.parse("(+ 21 42)") == ["(", "+", "21", "42", ")"]
-#
-#
-# def test_s_expression_arithmetic_nested():
-#     assert s_expression.parse("(+ 21 42 (+ 21 42))") == ["(", "+", "21", "42", ["(", "+", "21", "42", ")"], ")"]
-#
-#
-# def test_s_expression_identifier_simple():
-#     assert s_expression.parse("(foo 21 42)") == ["(", "foo", "21", "42", ")"]
-#
-#
-# def test_s_expression_identifier_nested():
-#     assert s_expression.parse("(foo 21 42 (bar 21 42))") == ["(", "foo", "21", "42", ["(", "bar", "21", "42", ")"], ")"]
-#
-#
-# def test_s_expression_floating_numbers():
-#     assert s_expression.parse("(foo 21.42 42.21)") == ["(", "foo", "21.42", "42.21", ")"]
-#
-#
-# def test_s_expression_rational_numbers():
-#     assert s_expression.parse("(foo 21/42 42/21)") == ["(", "foo", "21/42", "42/21", ")"]
