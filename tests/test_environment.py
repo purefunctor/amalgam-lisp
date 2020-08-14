cd117049a97ae3a801c5b65afc15b82dc0efcb85
PureFunctor <purefunctor@gmail.com>
2020-08-14 17:57:39 +0800

    Add tests for immediate environment dunder methods

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -55,3 +55,39 @@ def test_immediate_environment_iiter(immediate):
 
 def test_immediate_environment_ilen(immediate):
     assert immediate.ilen() == len(immediate.bindings)
+
+
+def test_immediate_environment_getitem(immediate):
+    assert immediate["foo"] == immediate.iget("foo")
+
+
+def test_immediate_environment_getitem_fails(immediate):
+    with raises(KeyError):
+        immediate["key-error"]
+
+
+def test_immediate_environment_settitem(immediate):
+    immediate["foo"] = 42
+    assert immediate.bindings.get("foo") == 42
+
+
+def test_immediate_environment_delitem(immediate):
+    del immediate["foo"]
+    assert immediate.bindings.get("foo", None) == None
+
+
+def test_immediate_environment_delitem_fails(immediate):
+    with raises(KeyError):
+        del immediate["key-error"]
+
+
+def test_immediate_environment_contains(immediate):
+    assert ("foo" in immediate) == ("foo" in immediate.bindings)
+
+
+def test_immediate_environment_iter(immediate):
+    assert list(iter(immediate)) == list(iter(immediate.bindings))
+
+
+def test_immediate_environment_len(immediate):
+    assert len(immediate) == len(immediate.bindings)
