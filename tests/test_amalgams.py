f3f017945960a035086a8d2ff934dd316f72f847
PureFunctor <purefunctor@gmail.com>
2020-08-19 15:19:40 +0800

    Move fixtures closer to their associated tests

--- tests/test_amalgams.py
+++ tests/test_amalgams.py
@@ -14,28 +14,13 @@ from amalgam.amalgams import (
 from pytest import fixture
 
 
-@fixture
-def num():
-    return Numeric(42)
+def _common_repr(inst: Amalgam):
+    return fr"<{inst.__class__.__name__} '[\s\S]+' @ {hex(id(inst))}>"
 
 
 @fixture
 def numerics():
     return Numeric(21), Numeric(21.42), Numeric(Fraction(21, 42))
-
-
-@fixture
-def strings():
-    return String(""), String("\n\t"), String("foo-bar-baz")
-
-
-@fixture
-def env():
-    return Environment()
-
-
-def _common_repr(inst: Amalgam):
-    return fr"<{inst.__class__.__name__} '[\s\S]+' @ {hex(id(inst))}>"
 
 
 def test_numeric_evaluate(numerics):
@@ -48,6 +33,11 @@ def test_numeric_repr(numerics):
         assert re.match(_common_repr(numeric), repr(numeric))
 
 
+@fixture
+def strings():
+    return String(""), String("\n\t"), String("foo-bar-baz")
+
+
 def test_string_evaluate(strings):
     for string in strings:
         assert string == string.evaluate(Environment())
@@ -56,6 +46,16 @@ def test_string_evaluate(strings):
 def test_string_repr(strings):
     for string in strings:
         assert re.match(_common_repr(string), repr(string))
+
+
+@fixture
+def num():
+    return Numeric(42)
+
+
+@fixture
+def env():
+    return Environment()
 
 
 def test_function_evaluate_binding(num, env):
