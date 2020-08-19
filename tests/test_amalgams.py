cadc5304b5432fafa5e7624b23ac8d22b860da09
PureFunctor <purefunctor@gmail.com>
2020-08-19 15:14:02 +0800

    Refactor tests for Numeric objects

--- tests/test_amalgams.py
+++ tests/test_amalgams.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 import re
 from typing import Any
 
@@ -11,20 +12,10 @@ from amalgam.amalgams import (
 )
 
 from hypothesis import given
-from hypothesis.strategies import (
-    integers, floats, fractions, text,
-    lists, one_of,
-)
+from hypothesis.strategies import text
 
 from pytest import fixture
 
-
-_non_scientific_float = floats(
-    allow_infinity=False,
-    allow_nan=False,
-).map(str).filter(lambda f: not re.search("[Ee][+-][0-9]+", f)).map(float)
-
-_numeric = one_of(integers(), _non_scientific_float, fractions()).map(Numeric)
 
 _string = text().map(String)
 
@@ -32,6 +23,11 @@ _string = text().map(String)
 @fixture
 def num():
     return Numeric(42)
+
+
+@fixture
+def numerics():
+    return Numeric(21), Numeric(21.42), Numeric(Fraction(21, 42))
 
 
 @fixture
@@ -43,14 +39,14 @@ def _common_repr(inst: Amalgam):
     return fr"<{inst.__class__.__name__} '[\s\S]+' @ {hex(id(inst))}>"
 
 
-@given(_numeric)
-def test_numeric_evaluate(numeric):
-    assert numeric == numeric.evaluate(Environment())
+def test_numeric_evaluate(numerics):
+    for numeric in numerics:
+        assert numeric == numeric.evaluate(Environment())
 
 
-@given(_numeric)
-def test_numeric_repr(numeric):
-    assert re.match(_common_repr(numeric), repr(numeric))
+def test_numeric_repr(numerics):
+    for numeric in numerics:
+        assert re.match(_common_repr(numeric), repr(numeric))
 
 
 @given(_string)
