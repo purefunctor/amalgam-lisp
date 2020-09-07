807bf1eb9f35943ff8fa745dc82616beb503ce16
PureFunctor <purefunctor@gmail.com>
2020-09-07 22:32:18 +0800

    Disable old parser implementation and tests

--- amalgam/parser.py
+++ amalgam/parser.py
@@ -1,45 +1,47 @@
-import parsy
-
-
-IDENTIFIER_PATTERN = r"(?!-?[0-9])[\+\-\*/\\&<=>?!_a-zA-Z0-9]+"
-
-
-@parsy.generate
-def numeric_literal():
-    """Parses a given numeric literal"""
-
-    integral_parser = parsy.regex(r"-?(0|[1-9][0-9]*)")
-
-    floating_parser = parsy.seq(
-        parsy.string("."),
-        parsy.regex("[0-9]+"),
-    ).concat()
-
-    fraction_parser = parsy.seq(
-        parsy.string("/"),
-        integral_parser,
-    ).concat()
-
-    head = yield integral_parser
-    tail = yield (floating_parser | fraction_parser).optional()
-
-    return head if tail is None else head + tail
-
-
-@parsy.generate
-def s_expression():
-    """Parses a given S-Expression"""
-
-    identifier = parsy.regex(IDENTIFIER_PATTERN)
-
-    l_paren = yield parsy.string("(")
-
-    f = yield identifier
-
-    yield parsy.whitespace
-
-    v = yield (identifier | numeric_literal | s_expression).sep_by(parsy.whitespace)
-
-    r_paren = yield parsy.string(")")
-
-    return [l_paren, f, *v, r_paren]
+# import parsy
+#
+#
+# IDENTIFIER_PATTERN = r"(?!-?[0-9])[\+\-\*/\\&<=>?!_a-zA-Z0-9]+"
+#
+#
+# @parsy.generate
+# def numeric_literal():
+#     """Parses a given numeric literal"""
+#
+#     integral_parser = parsy.regex(r"-?(0|[1-9][0-9]*)")
+#
+#     floating_parser = parsy.seq(
+#         parsy.string("."),
+#         parsy.regex("[0-9]+"),
+#     ).concat()
+#
+#     fraction_parser = parsy.seq(
+#         parsy.string("/"),
+#         integral_parser,
+#     ).concat()
+#
+#     head = yield integral_parser
+#     tail = yield (floating_parser | fraction_parser).optional()
+#
+#     return head if tail is None else head + tail
+#
+#
+# @parsy.generate
+# def s_expression():
+#     """Parses a given S-Expression"""
+#
+#     identifier = parsy.regex(IDENTIFIER_PATTERN)
+#
+#     l_paren = yield parsy.string("(")
+#
+#     f = yield identifier
+#
+#     yield parsy.whitespace
+#
+#     v = yield (identifier | numeric_literal | s_expression).sep_by(
+#       parsy.whitespace
+#     )
+#
+#     r_paren = yield parsy.string(")")
+#
+#     return [l_paren, f, *v, r_paren]
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,70 +1,70 @@
-import re
-
-from amalgam.parser import numeric_literal, s_expression
-from amalgam.parser import IDENTIFIER_PATTERN
-
-from hypothesis import assume, given
-from hypothesis.strategies import (
-    integers, floats, fractions, from_regex,
-    composite, lists, one_of,
-)
-
-
-_identifier = from_regex(fr"\A{IDENTIFIER_PATTERN}\Z")
-
-_integral = integers().map(str)
-
-_floating = floats(
-    allow_infinity=False,
-    allow_nan=False,
-).map(str).filter(lambda f: not re.search("[Ee][+-][0-9]+", f))
-
-_fraction = fractions().map(str)
-
-
-@composite
-def _arbitrary_s_expressions(draw):
-    identifier = draw(_identifier)
-
-    literals = draw(lists(
-        one_of(_integral, _floating, _fraction, _identifier)
-    ).filter(lambda l: len(l) > 0))
-
-    return f"({identifier} {' '.join(literals)})", ["(", identifier, *literals, ")"]
-
-
-@given(_identifier)
-def test_identifier_pattern(identifier):
-    assert not re.match("^-?[0-9]", identifier)
-
-
-@given(_integral)
-def test_numeric_literal_integral(integral):
-    assert numeric_literal.parse(integral) == integral
-
-
-@given(_floating)
-def test_numeric_literal_floating(floating):
-    assert numeric_literal.parse(floating) == floating
-
-
-@given(_fraction)
-def test_numeric_literal_fraction(fraction):
-    assert numeric_literal.parse(fraction) == fraction
-
-
-@given(_arbitrary_s_expressions())
-def test_s_expression_simple(data):
-    expression, expected = data
-    assert s_expression.parse(expression) == expected
-
-
-@given(_arbitrary_s_expressions(), _arbitrary_s_expressions())
-def test_s_expression_nested(l, r):
-    ls, le = l
-    rs, re = r
-
-    ts = f"(T {ls} {rs})"
-    te = ["(", "T", le, re, ")"]
-
-    assert s_expression.parse(ts) == te
+# import re
+#
+# from amalgam.parser import numeric_literal, s_expression
+# from amalgam.parser import IDENTIFIER_PATTERN
+#
+# from hypothesis import assume, given
+# from hypothesis.strategies import (
+#     integers, floats, fractions, from_regex,
+#     composite, lists, one_of,
+# )
+#
+#
+# _identifier = from_regex(fr"\A{IDENTIFIER_PATTERN}\Z")
+#
+# _integral = integers().map(str)
+#
+# _floating = floats(
+#     allow_infinity=False,
+#     allow_nan=False,
+# ).map(str).filter(lambda f: not re.search("[Ee][+-][0-9]+", f))
+#
+# _fraction = fractions().map(str)
+#
+#
+# @composite
+# def _arbitrary_s_expressions(draw):
+#     identifier = draw(_identifier)
+#
+#     literals = draw(lists(
+#         one_of(_integral, _floating, _fraction, _identifier)
+#     ).filter(lambda l: len(l) > 0))
+#
+#     return f"({identifier} {' '.join(literals)})", ["(", identifier, *literals, ")"]
+#
+#
+# @given(_identifier)
+# def test_identifier_pattern(identifier):
+#     assert not re.match("^-?[0-9]", identifier)
+#
+#
+# @given(_integral)
+# def test_numeric_literal_integral(integral):
+#     assert numeric_literal.parse(integral) == integral
+#
+#
+# @given(_floating)
+# def test_numeric_literal_floating(floating):
+#     assert numeric_literal.parse(floating) == floating
+#
+#
+# @given(_fraction)
+# def test_numeric_literal_fraction(fraction):
+#     assert numeric_literal.parse(fraction) == fraction
+#
+#
+# @given(_arbitrary_s_expressions())
+# def test_s_expression_simple(data):
+#     expression, expected = data
+#     assert s_expression.parse(expression) == expected
+#
+#
+# @given(_arbitrary_s_expressions(), _arbitrary_s_expressions())
+# def test_s_expression_nested(l, r):
+#     ls, le = l
+#     rs, re = r
+#
+#     ts = f"(T {ls} {rs})"
+#     te = ["(", "T", le, re, ")"]
+#
+#     assert s_expression.parse(ts) == te
