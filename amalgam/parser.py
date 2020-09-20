dd0dcd90c8d4a936fc6ae15881241995ff7ec314
PureFunctor <purefunctor@gmail.com>
2020-09-20 16:28:45 +0800

    Add apply_splat helper function

--- amalgam/parser.py
+++ amalgam/parser.py
@@ -1,8 +1,24 @@
 from fractions import Fraction
+from functools import wraps
 
 import pyparsing as pp
 
 import amalgam.amalgams as am
+
+
+def apply_splat(func):
+    """
+    Helper function for splatting args to callbacks.
+
+    Takes a callable `func` and return a function of one
+    argument `tokens` and splats `tokens` into `func`.
+    """
+
+    @wraps(func)
+    def _(tokens):
+        return func(*tokens)
+
+    return _
 
 
 LPAREN, RPAREN, LBRACE, RBRACE = map(pp.Suppress, "()[]")
@@ -10,37 +26,37 @@ LPAREN, RPAREN, LBRACE, RBRACE = map(pp.Suppress, "()[]")
 symbol_parser = pp.Regex(
     r"(?!-?[0-9])[\+\-\*/\\&<=>?!_a-zA-Z0-9]+"
 ).setParseAction(
-    lambda tokens: am.Symbol(*tokens)
+    apply_splat(am.Symbol)
 )
 
 string_parser = pp.QuotedString(
     "\"", convertWhitespaceEscapes=False,
 ).setParseAction(
-    lambda tokens: am.String(*tokens)
+    apply_splat(am.String)
 )
 
 _string_integral_parser = pp.Regex(r"-?(0|[1-9]\d*)")
 
 _integral_parser = _string_integral_parser.copy().setParseAction(
-    lambda tokens: int(*tokens)
+    apply_splat(int)
 )
 
 _floating_parser = pp.Combine(
     _string_integral_parser + pp.Literal(".") + pp.Regex(r"\d+")
 ).setParseAction(
-    lambda tokens: float(*tokens)
+    apply_splat(float)
 )
 
 _fraction_parser = (
     _integral_parser + pp.Suppress("/") + _integral_parser
 ).setParseAction(
-    lambda tokens: Fraction(*tokens)
+    apply_splat(Fraction)
 )
 
 numeric_parser = (
     _floating_parser | _fraction_parser | _integral_parser
 ).setParseAction(
-    lambda tokens: am.Numeric(*tokens)
+    apply_splat(am.Numeric)
 )
 
 expression_parser = pp.Forward()
@@ -48,19 +64,19 @@ expression_parser = pp.Forward()
 quoted_parser = (
     pp.Suppress("'") + expression_parser
 ).setParseAction(
-    lambda tokens: am.Quoted(*tokens)
+    apply_splat(am.Quoted)
 )
 
 s_expression_parser = (
     LPAREN + expression_parser[...] + RPAREN
 ).setParseAction(
-    lambda tokens: am.SExpression(*tokens)
+    apply_splat(am.SExpression)
 )
 
 vector_parser = (
     LBRACE + expression_parser[...] + RBRACE
 ).setParseAction(
-    lambda tokens: am.Vector(*tokens)
+    apply_splat(am.Vector)
 )
 
 expression_parser <<= (
