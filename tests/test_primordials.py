e7024852d8ddac735610ff47a6314cdeb11b4377
PureFunctor <purefunctor@gmail.com>
2020-10-04 01:46:13 +0800

    Add parameterized test for arithmetic functions

---
+++ tests/test_primordials.py
@@ -0,0 +1,29 @@
+from amalgam.amalgams import Environment, Numeric
+from amalgam.primordials import _add, _sub, _mul, _div
+
+from pytest import mark, param
+
+
+arithmetics = (
+    param(
+        arith_fn,
+        arith_nm,
+        arith_rs,
+        id=f"({arith_op} {' '.join(map(str, arith_nm))})"
+    )
+    for arith_fn, arith_op, arith_nm, arith_rs in (
+        (_add, "+", (1, 2), 1 + 2),
+        (_add, "+", (1, 2, 3), 1 + 2 + 3),
+        (_sub, "-", (5, 3), 5 - 3),
+        (_sub, "-", (3, 5, 3), 3 - (5 + 3)),
+        (_mul, "*", (2, 2), 2 * 2),
+        (_mul, "*", (2, 2, 2), 2 * 2 * 2),
+        (_div, "/", (4, 2), 4 / 2),
+        (_div, "/", (6, 2, 3), 6 / (2 * 3)),
+    )
+)
+
+
+@mark.parametrize(("arith_fn", "arith_nm", "arith_rs"), arithmetics)
+def test_arithmetic_function(arith_fn, arith_nm, arith_rs):
+    assert arith_fn(Environment(), *map(Numeric, arith_nm)).value == arith_rs
