7f40f9081c9164a7b1c5e675728854b96fe2914e
PureFunctor <purefunctor@gmail.com>
2020-10-03 21:15:47 +0800

    Add basic arithmetic functions

--- amalgam/primordials.py
+++ amalgam/primordials.py
@@ -1,9 +1,12 @@
+from fractions import Fraction
 from functools import partial
 from typing import Callable, Dict, TypeVar, Union
 
 from amalgam.amalgams import (
     Amalgam,
+    Environment,
     Function,
+    Numeric,
 )
 
 
@@ -29,3 +32,34 @@ def _make_function(
     FUNCTIONS[name] = Function(name, func, defer)
 
     return func
+
+
+@_make_function("+")
+def _add(_env: Environment, *nums: Numeric) -> Numeric:
+    return Numeric(sum(num.value for num in nums))
+
+
+@_make_function("-")
+def _sub(_env: Environment, *nums: Numeric) -> Numeric:
+    x, *ns = (num.value for num in nums)
+    y: Union[float, Fraction] = 0
+    for n in ns:
+        y += n
+    return Numeric(x - y)
+
+
+@_make_function("*")
+def _mul(_env: Environment, *nums: Numeric) -> Numeric:
+    prod: Union[float, Fraction] = 1
+    for num in nums:
+        prod *= num.value
+    return Numeric(prod)
+
+
+@_make_function("/")
+def _div(_env: Environment, *nums: Numeric) -> Numeric:
+    x, *ns = (num.value for num in nums)
+    y: Union[float, Fraction] = 1
+    for n in ns:
+        y *= n
+    return Numeric(x / y)
