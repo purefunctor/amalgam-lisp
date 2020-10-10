a7d3364cff4c387bd8c377c499e26fbf1ce39446
PureFunctor <purefunctor@gmail.com>
2020-10-10 16:36:58 +0800

    Add effectful tests for _setn, _fn, and _mkfn

--- tests/test_primordials.py
+++ tests/test_primordials.py
@@ -1,5 +1,15 @@
-from amalgam.amalgams import Environment, Numeric
+from amalgam.amalgams import (
+    Environment,
+    Function,
+    Numeric,
+    Quoted,
+    SExpression,
+    String,
+    Symbol,
+    Vector,
+)
 from amalgam.primordials import (
+    FUNCTIONS,
     _add,
     _sub,
     _mul,
@@ -9,7 +19,13 @@ from amalgam.primordials import (
     _mkfn,
 )
 
-from pytest import mark, param
+from pytest import fixture, mark, param
+
+
+@fixture
+def env():
+    return Environment(None, FUNCTIONS)
+
 
 arithmetics = (
     param(
@@ -34,3 +50,33 @@ arithmetics = (
 @mark.parametrize(("arith_fn", "arith_nm", "arith_rs"), arithmetics)
 def test_arithmetic_function(arith_fn, arith_nm, arith_rs):
     assert arith_fn(Environment(), *map(Numeric, arith_nm)).value == arith_rs
+
+
+def test_setn(env):
+    name = Symbol("name")
+    amalgam = String("string")
+
+    _setn_result = _setn(env, Quoted(name), Quoted(amalgam))
+
+    assert env.iget("name") == amalgam
+    assert _setn_result == amalgam
+
+
+def test_fn(env):
+    args = Vector(Symbol("x"), Symbol("y"))
+    body = SExpression(Symbol("+"), Symbol("x"), Symbol("y"))
+
+    _fn_result = _fn(env, Quoted(args), Quoted(body))
+
+    assert _fn_result.fn(env, Numeric(21), Numeric(21)) == Numeric(42)
+
+
+def test_mkfn(env):
+    name = Symbol("name")
+    args = Vector(Symbol("x"), Symbol("y"))
+    body = SExpression(Symbol("+"), Symbol("x"), Symbol("y"))
+
+    _mkfn_result = _mkfn(env, Quoted(name), Quoted(args), Quoted(body))
+
+    assert env.iget("name") == _mkfn_result
+    assert _mkfn_result.fn(env, Numeric(21), Numeric(21)) == Numeric(42)
