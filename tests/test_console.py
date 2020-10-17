ff8c7d2fe487d77b874de1d237666d2670b94af3
PureFunctor <purefunctor@gmail.com>
2020-10-17 15:36:42 +0800

    Add tests for AmalgamREPL

---
+++ tests/test_console.py
@@ -0,0 +1,45 @@
+from amalgam.repl import AmalgamREPL
+
+from pytest import raises
+
+
+def test_repl(mocker, capsys):
+    MockClassPromptSession = mocker.Mock()
+    MockSelfPromptSession = mocker.Mock()
+
+    MockClassPromptSession.return_value = MockSelfPromptSession
+    MockSelfPromptSession.prompt.side_effect = (
+        "(+ 42 42)",
+
+        "(+ 1",
+        "2 3)",
+
+        "[1 2 3 4]",
+
+        "[4 3",
+        "2 1]",
+
+        "(/ 1 0)",
+
+        "(/",
+        " 1",
+        " 0",
+        ")",
+
+        "(exit 0)",
+    )
+
+    mocker.patch("amalgam.repl.PromptSession", MockClassPromptSession)
+
+    with raises(SystemExit):
+        AmalgamREPL().repl()
+
+    assert capsys.readouterr().out == (
+        "84\n"
+        "6\n"
+        "[1 2 3 4]\n"
+        "[4 3 2 1]\n"
+        "ZeroDivisionError: division by zero\n"
+        "ZeroDivisionError: division by zero\n"
+        "Goodbye.\n"
+    )
