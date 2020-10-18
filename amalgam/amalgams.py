cb2ad2aac15d9c994fbd6b4c9a6c4d34dca59317
PureFunctor <purefunctor@gmail.com>
2020-10-18 19:20:46 +0800

    Implement and test a new Environment class

--- amalgam/amalgams.py
+++ amalgam/amalgams.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from contextlib import contextmanager
 from dataclasses import dataclass
 from fractions import Fraction
 from itertools import chain
@@ -11,6 +12,7 @@ from typing import (
     Dict,
     Iterator,
     Generic,
+    Mapping,
     MutableMapping,
     Optional,
     Tuple,
@@ -356,3 +358,190 @@ class Environment(MutableMapping[str, Amalgam]):
             return self.parent
         else:
             raise Exception("Cannot discard top-level `Environment`")
+
+
+_Bindings = Mapping[str, Amalgam]
+_MutBindings = MutableMapping[str, Amalgam]
+
+
+class SymbolNotFound(Exception):
+    """Synonym for `KeyError`."""
+
+
+class TopLevelPop(Exception):
+    """Raised at `_Environment.env_pop`"""
+
+
+class _Environment:
+    """
+    Class that manages and represents nested execution environments.
+    """
+
+    def __init__(
+        self,
+        bindings: _Bindings = None,
+        parent: _Environment = None,
+    ) -> None:
+        self.bindings: _MutBindings = {**bindings} if bindings else {}
+        self.parent: Optional[_Environment] = parent
+        self.level: int = parent.level + 1 if parent else 0
+        self.search_depth: int = 0
+
+    def __getitem__(self, item: str) -> Amalgam:
+        """
+        Attempts to recursively obtain the provided `item`.
+
+        Searches with respect to the current `search_depth` attribute
+        of the calling `_Environment` instance. If an existing `item`
+        if encountered at a certain depth less than the target depth,
+        returns that `item`, otherwise, raises `SymbolNotFound`.
+        """
+        _self = self
+
+        if self.search_depth >= 0:
+            depth = self.search_depth + 1
+        else:
+            depth = self.level + 1
+
+        # Search until the top-most environment,
+        # raise SymbolNotFound otherwise.
+        for _ in range(depth):
+            try:
+                return _self.bindings[item]
+            except KeyError:
+                _self = cast(_Environment, _self.parent)
+        else:
+            raise SymbolNotFound(item)
+
+    def __setitem__(self, item: str, value: Amalgam) -> None:
+        """
+        Attempts to recursively set the provided `value` to an `item`.
+
+        Searches with respect to the current `search_depth` attribute
+        of the calling `_Environment` instance. If an existing `item`
+        is encountered at a certain depth less than the target depth,
+        overrides that `item` instead.
+        """
+        _self = self
+
+        if self.search_depth >= 0:
+            depth = self.search_depth
+        else:
+            depth = self.level
+
+        # Search until the second-to-last environment,
+        # set at the top-most environment otherwise.
+        for _ in range(depth):
+            if item in _self.bindings:
+                _self.bindings[item] = value
+                break
+            else:
+                _self = cast(_Environment, _self.parent)
+        else:
+            _self.bindings[item] = value
+
+    def __delitem__(self, item: str) -> None:
+        """
+        Attempts to recursively delete the provided `item`.
+
+        Searches with respect to the current `search_depth` attribute
+        of the calling `_Environment` instance. If an existing `item`
+        is encountered at a certain depth less than the target depth,
+        deletes that `item` instead.
+        """
+        _self = self
+
+        if self.search_depth >= 0:
+            depth = self.search_depth + 1
+        else:
+            depth = self.level + 1
+
+        # Search until the top-most environment,
+        # raise SymbolNotFound otherwise.
+        for _ in range(depth):
+            try:
+                del _self.bindings[item]
+                break
+            except KeyError:
+                _self = cast(_Environment, _self.parent)
+        else:
+            raise SymbolNotFound(item)
+
+    def __contains__(self, item: str) -> bool:
+        """
+        Recursively checks whether an `item` exists.
+
+        Searches with respect to the current `search_depth` attribute
+        of the calling `Environment` instance. If the target `item` is
+        encountered at a certain depth less than the target depth,
+        immediately returns True, otherwise, returns False.
+        """
+        _self = self
+
+        if self.search_depth >= 0:
+            depth = self.search_depth + 1
+        else:
+            depth = self.level + 1
+
+        # Search until an `item` is found,
+        # return False otherwise
+        for _ in range(depth):
+            if item in _self.bindings:
+                return True
+            else:
+                _self = cast(Environment, _self.parent)
+        else:
+            return False
+
+    @contextmanager
+    def search_at(self, *, depth=0):
+        """
+        Context manager for temporarily setting the lookup depth.
+
+        The provided `depth` argument must not exceed the `level`
+        attribute of the calling `_Environment` instance, and will
+        raise a `ValueError` if done so.
+
+        >>> env = _Environment(FUNCTIONS)
+        >>>
+        >>> with env.search_at(depth=42):
+        ...     env["+"]  # Raises ValueError
+
+        Any negative integer can be passed as a `depth` to signify
+        an infinite lookup until the top-most environment.
+
+        >>> env = _Environment(FUNCTIONS)
+        >>> cl_env = env.env_push({...})
+        >>>
+        >>> with cl_env.search_at(depth=-1):
+        ...    cl_env["+"]  # Searches `env`
+        """
+        if depth > self.level:
+            exc = ValueError(
+                f"depth {depth} is greater than maximum level {self.level}"
+            )
+            raise exc
+
+        self.search_depth = depth
+
+        try:
+            yield self
+        finally:
+            self.search_depth = 0
+
+    def env_push(self, bindings: _Bindings = None) -> _Environment:
+        """
+        Creates a new `_Environment` and binds the calling instance
+        as its parent environment.
+        """
+        return _Environment(bindings, self)
+
+    def env_pop(self) -> _Environment:
+        """
+        Discards the current environment and returns the parent
+        environment.
+        """
+        if self.parent is not None:
+            return self.parent
+        else:
+            raise TopLevelPop("cannot discard top-level _Environment")
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,7 +1,13 @@
 from typing import Any
 from itertools import chain
 
-from amalgam.amalgams import Amalgam, Environment
+from amalgam.amalgams import (
+    Amalgam,
+    Environment,
+    _Environment,
+    SymbolNotFound,
+    TopLevelPop,
+)
 
 from pytest import fixture, raises
 
@@ -197,3 +203,170 @@ def test_nested_environment_push(nested):
 
 def test_nested_environment_pop(nested):
     assert nested.env_pop() == nested.parent
+
+
+@fixture
+def flat_environment():
+    return _Environment({"foo": 21, "bar": 42})
+
+
+@fixture
+def nested_environment():
+    return _Environment(
+        {"foo": 21, "bar": 42}
+    ).env_push(
+        {"baz": 63, "nil": 84}
+    ).env_push(
+        {"not": 105, "nul": 126}
+    )
+
+
+def test_environment_copies_bindings():
+    bindings = {"x": 21, "y": 42}
+    assert _Environment(bindings).bindings is not bindings
+
+
+def test_environment_increments_level(flat_environment):
+    assert flat_environment.level == 0
+    assert _Environment(parent=flat_environment).level == 1
+
+
+def test_environment_env_push(flat_environment):
+    bindings = {"x": 21, "y": 42}
+    new_env = flat_environment.env_push(bindings)
+
+    assert new_env.parent == flat_environment
+    assert new_env.bindings == bindings
+    assert new_env.level == flat_environment.level + 1
+
+
+def test_environment_env_pop(flat_environment):
+    assert flat_environment.env_push().env_pop() is flat_environment
+
+    with raises(TopLevelPop):
+        flat_environment.env_pop()
+
+
+def test_environment_search_at(nested_environment):
+    with nested_environment.search_at(depth=1):
+        assert nested_environment.search_depth == 1
+    assert nested_environment.search_depth == 0
+
+    with nested_environment.search_at(depth=-1):
+        assert nested_environment.search_depth == -1
+    assert nested_environment.search_depth == 0
+
+    with raises(ValueError):
+        with nested_environment.search_at(depth=3):
+            pass
+
+
+def test_environment_getitem_immediate(flat_environment):
+    assert flat_environment["foo"] == flat_environment.bindings["foo"]
+
+    with raises(SymbolNotFound):
+        flat_environment["nil"]
+
+
+def test_environment_setitem_immediate(flat_environment):
+    flat_environment["foo"] = 84
+    flat_environment["new"] = 42
+
+    assert flat_environment.bindings["foo"] == 84
+    assert flat_environment.bindings["new"] == 42
+
+
+def test_environment_delitem_immediate(flat_environment):
+    del flat_environment["foo"]
+
+    assert "foo" not in flat_environment.bindings
+
+    with raises(SymbolNotFound):
+        del flat_environment["nil"]
+
+
+def test_environment_contains_immediate(flat_environment):
+    assert "foo" in flat_environment
+    assert "new" not in flat_environment
+
+
+def test_environment_getitem_infinite(nested_environment):
+    with nested_environment.search_at(depth=-1):
+        assert nested_environment["nul"] == nested_environment.bindings["nul"]
+        assert nested_environment["baz"] == nested_environment.parent.bindings["baz"]
+        assert nested_environment["foo"] == nested_environment.parent.parent.bindings["foo"]
+
+    with raises(SymbolNotFound):
+        with nested_environment.search_at(depth=-1):
+            nested_environment["new"]
+
+
+def test_environment_setitem_infinite(nested_environment):
+    with nested_environment.search_at(depth=-1):
+        nested_environment["nul"] = 21
+        nested_environment["baz"] = 42
+        nested_environment["foo"] = 63
+
+    assert nested_environment.bindings["nul"] == 21
+    assert nested_environment.parent.bindings["baz"] == 42
+    assert nested_environment.parent.parent.bindings["foo"] == 63
+
+
+def test_environment_delitem_infinite(nested_environment):
+    with nested_environment.search_at(depth=-1):
+        del nested_environment["nul"]
+        del nested_environment["baz"]
+        del nested_environment["foo"]
+
+    assert "nul" not in nested_environment.bindings
+    assert "baz" not in nested_environment.parent.bindings
+    assert "foo" not in nested_environment.parent.parent.bindings
+
+
+def test_environment_contains_infinite(nested_environment):
+    with nested_environment.search_at(depth=-1):
+        assert "nul" in nested_environment
+        assert "baz" in nested_environment
+        assert "foo" in nested_environment
+        assert "new" not in nested_environment
+
+
+def test_environment_getitem_limited(nested_environment):
+    with nested_environment.search_at(depth=1):
+        assert nested_environment["nul"] == nested_environment.bindings["nul"]
+        assert nested_environment["baz"] == nested_environment.parent.bindings["baz"]
+
+    with nested_environment.search_at(depth=1):
+        with raises(SymbolNotFound):
+            nested_environment["foo"]
+
+
+def test_environment_setitem_limited(nested_environment):
+    with nested_environment.search_at(depth=1):
+        nested_environment["nul"] = 21
+        nested_environment["baz"] = 42
+        nested_environment["foo"] = 63
+
+    assert nested_environment.bindings["nul"] == 21
+    assert nested_environment.parent.bindings["baz"] == 42
+    assert nested_environment.parent.bindings["foo"] == 63
+
+
+def test_environment_delitem_limited(nested_environment):
+    with nested_environment.search_at(depth=1):
+        del nested_environment["nul"]
+        del nested_environment["baz"]
+
+    assert "nul" not in nested_environment.bindings
+    assert "baz" not in nested_environment.parent.bindings
+
+    with nested_environment.search_at(depth=1):
+        with raises(SymbolNotFound):
+            del nested_environment["foo"]
+
+
+def test_environment_contains_limited(nested_environment):
+    with nested_environment.search_at(depth=1):
+        assert "nul" in nested_environment
+        assert "baz" in nested_environment
+        assert "foo" not in nested_environment
