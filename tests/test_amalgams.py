1b2f3bf324ebee3ae6f4a536315878fabb7c8187
PureFunctor <purefunctor@gmail.com>
2020-10-19 16:28:47 +0800

    Simplify tests for create_fn and closure_fn

--- tests/test_amalgams.py
+++ tests/test_amalgams.py
@@ -15,21 +15,6 @@ from amalgam.primordials import FUNCTIONS
 
 from pytest import fixture, mark, param
 
-from tests.utils import (
-    MockAmalgam,
-    MockEnvironment,
-)
-
-
-@fixture
-def mock_environment():
-    return MockEnvironment()
-
-
-@fixture
-def mock_fn(mocker):
-    return mocker.MagicMock(return_value=mocker.MagicMock())
-
 
 @fixture
 def env():
@@ -128,39 +113,40 @@ def test_function_call_env_override(env, mocker):
     fn.assert_called_once_with(env, sym)
 
 
-def test_create_fn(mocker, mock_fn):
-    mock_fname = mocker.MagicMock()
-    mock_defer = mocker.MagicMock()
+def test_create_fn_simple(env):
+    function = create_fn(
+        "create_fn-simple-test",
+        ["x", "y"],
+        Vector(Symbol("x"), Symbol("y")),
+        False,
+    )
 
-    mocker.patch("amalgam.amalgams.Function", mock_fn)
+    _21 = Numeric(21)
+    vec = Vector(_21, _21)
 
-    create_fn_result = create_fn(mock_fname, mocker.MagicMock(), mocker.MagicMock(), mock_defer)
+    assert function.name == "create_fn-simple-test"
+    assert function.defer == False
+    assert function.call(env, _21, _21) == vec
 
-    mock_fn.assert_called_once_with(mock_fname, mocker.ANY, mock_defer)
-    assert create_fn_result == mock_fn.return_value
 
+def test_create_fn_closure(env):
+    function = create_fn(
+        "create_fn-simple-closure",
+        ["x"],
+        create_fn(
+            "~lambda~",
+            ["y"],
+            Vector(Symbol("x"), Symbol("y")),
+            False,
+        ),
+        False
+    )
 
-def test_closure_fn(mocker, mock_environment, mock_fn):
-    mock_fargs = mocker.MagicMock()
-    mock_fargs.__iter__.return_value = (mocker.Mock(), mocker.Mock())
+    _21 = Numeric(21)
+    vec = Vector(_21, _21)
 
-    mock_args = mocker.MagicMock()
-    mock_args.__iter__.return_value = (mocker.Mock(), mocker.Mock())
-
-    mock_fbody = MockAmalgam()
-    mock_fbody_result = MockAmalgam()
-    mock_fbody.evaluate.return_value = mock_fbody_result
-
-    mock_child_environment = MockEnvironment()
-    mock_environment.env_push.return_value = mock_child_environment
-
-    mocker.patch("amalgam.amalgams.Function", mock_fn)
-    create_fn(mocker.MagicMock(), mock_fargs, mock_fbody, mocker.MagicMock())
-    _, closure_fn, _ = mock_fn.call_args[0]
-
-    closure_fn_result = closure_fn(mock_environment, *mock_args)
-
-    mock_environment.env_push.assert_called_once_with(dict(zip(mock_fargs, mock_args)))
-    mock_fbody.evaluate.assert_called_once_with(mock_child_environment)
-    mock_fbody_result.bind.assert_called_once_with(mock_child_environment)
-    assert closure_fn_result == mock_fbody_result
+    closure = function.call(env, _21)
+    assert closure.name == "~lambda~"
+    assert closure.env is not None
+    assert closure.defer == False
+    assert closure.call(env, _21) == vec
