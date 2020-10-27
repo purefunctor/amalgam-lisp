aa9b82689e4f79b008c56c38cc16eb9d2f240879
PureFunctor <purefunctor@gmail.com>
2020-10-27 14:43:12 +0800

    Add package metadata and documentation

--- amalgam/__init__.py
+++ amalgam/__init__.py
@@ -1,1 +1,7 @@
+"""LISP-like interpreted language implemented in Python."""
+
+__title__ = "amalgam"
+__author__ = "PureFunctor"
+__copyright__ = "Copyright 2020 PureFunctor"
+__license__ = "MIT"
 __version__ = "1.0.0"
