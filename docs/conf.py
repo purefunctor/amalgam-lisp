bca8cd864e7fd618dd20e989fd16409d4ccba663
PureFunctor <purefunctor@gmail.com>
2020-11-01 16:47:51 +0800

    Bump package version down to 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, PureFunctor'
 author = 'PureFunctor'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.0'
+release = 'v0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
--- tests/test_amalgam.py
+++ tests/test_amalgam.py
@@ -2,4 +2,4 @@ from amalgam import __version__
 
 
 def test_amalgam():
-    assert __version__ == "1.0.0"
+    assert __version__ == "0.1.0"
