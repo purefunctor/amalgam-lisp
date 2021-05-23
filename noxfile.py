3d27e14f0cef0321c3f5ed1e77b1e657b8057339
PureFunctor <purefunctor@gmail.com>
2021-05-24 04:49:45 +0800

    Use --without-hashes for generating constraints (#3)

--- noxfile.py
+++ noxfile.py
@@ -14,6 +14,7 @@ def install_with_constraints(session, *args, **kwargs):
             "--dev",
             "--format=requirements.txt",
             f"--output={requirements.name}",
+            "--without-hashes",
             external=True,
         )
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
