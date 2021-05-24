03a8d6e440b75a6e2a00e209f1b3df09b998d621
PureFunctor <purefunctor@gmail.com>
2021-05-24 22:01:33 +0800

    Simplify make_function implementation

--- amalgam/primordials/utils.py
+++ amalgam/primordials/utils.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
-from functools import partial, wraps
+from functools import wraps
+from mypy_extensions import Arg, KwArg, VarArg
 from typing import (
     cast,
-    overload,
     Callable,
     List,
     MutableMapping,
     Sequence,
     TypeVar,
-    Union,
     TYPE_CHECKING,
 )
 
@@ -21,72 +20,55 @@ if TYPE_CHECKING:  # pragma: no cover
 
     Store = MutableMapping[str, am.Function]
 
+    T = TypeVar("T", bound=am.Amalgam)
 
-T = TypeVar("T", bound=am.Amalgam)
+    _Function = Callable[
+        [
+            Arg(Environment, "env"),  # noqa: F821
+            VarArg(am.Amalgam),
+            KwArg(am.Amalgam),
+        ],
+        T
+    ]
 
 
-@overload
 def make_function(
     store: Store,
     name: str,
-    func: Callable[..., T],
     defer: bool = False,
     contextual: bool = False,
     allows: Sequence[str] = None,
-) -> Callable[..., T]:  # pragma: no cover
-    ...
-
-
-@overload
-def make_function(
-    store: Store,
-    name: str,
-    func: None = None,
-    defer: bool = False,
-    contextual: bool = False,
-    allows: Sequence[str] = None,
-) -> partial:  # pragma: no cover
-    ...
-
-
-def make_function(
-    store, name, func=None, defer=False, contextual=False, allows=None,
-) -> Union[partial, Callable[..., T]]:
+) -> Callable[[Callable[..., T]], _Function[T]]:
     """
-    Transforms a :data:`func` into a :class:`.amalgams.Function` and
-    places it inside of a :data:`store`.
+    Decorator factory for transforming some callable into a
+    :class:`.amalgams.Function`, injecting it into a :data:`store`.
     """
 
-    if func is None:
-        return partial(
-            make_function,
-            store,
-            name,
-            defer=defer,
-            contextual=contextual,
-            allows=allows,
-        )
+    _allows = allows or []
 
-    if allows is None:
-        allows = []
+    def _make_function(f: Callable[..., T]) -> _Function[T]:
+        @wraps(f)
+        def _f(
+            env: Environment,
+            *arguments: am.Amalgam,
+            **keywords: am.Amalgam,
+        ) -> T:
 
-    @wraps(func)
-    def _func(
-        env: Environment, *arguments: am.Amalgam, **keywords: am.Amalgam
-    ) -> am.Amalgam:
-        with env.search_at(depth=-1):
-            fns = cast(List[am.Function], [env[allow] for allow in allows])
+            with env.search_at(depth=-1):
+                fns = cast(List[am.Function], [env[allow] for allow in _allows])
 
-        for fn in fns:
-            fn.in_context = True
+            for fn in fns:
+                fn.in_context = True
 
-        result = func(env, *arguments, **keywords)
+            result = f(env, *arguments, **keywords)
 
-        for fn in fns:
-            fn.in_context = False
+            for fn in fns:
+                fn.in_context = False
 
-        return result
+            return result
 
-    store[name] = am.Function(name, _func, defer, contextual)
+        store[name] = am.Function(name, _f, defer, contextual)
 
-    return _func
+        return _f
+
+    return _make_function
